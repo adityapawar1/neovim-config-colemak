ab810b80d20f8043a38ee139a36d9ebdb3f1bb51
Aditya Pawar <aditya.pawar@warriorlife.net>
2022-12-02 19:42:43 -0800

    Edit lsps

--- plugins/lspconfig.lua
+++ plugins/lspconfig.lua
@@ -15,14 +15,14 @@
 -- end
 
 -- Safely import lspconfig
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
 local present, lspconfig = pcall(require, "lspconfig")
 if not present then
 	warn("LspConfig not installed")
 	return
 end
--- use nvchad's setup lsp functions
-local attach = require("plugins.configs.lspconfig").on_attach
-local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local servers = {
 	"cssls",
@@ -32,33 +32,18 @@ local servers = {
 	"tsserver",
 	"pyright",
   -- "autopep8",
-  -- "elixir-ls",
+  "elixir-ls",
   -- "prettier",
   "prismals",
 	"rust_analyzer",
 	"tailwindcss",
   "jdtls",
-  -- "r-languageserver"
+  "r-languageserver"
 }
--- local servers = { "html", "cssls", "bashls", "emmet_ls", "tsserver", "pyright" }
 
-local on_attach = function(client, bufnr)
-	attach(client, bufnr)
-	-- TODO check if this does anything
-	-- lsp_highlight_document(client)
-end
-
--- bind functions for each language server
 for _, lsp in ipairs(servers) do
-	local opts = {
-		on_attach = on_attach,
-		capabilities = capabilities,
-	}
-	local ok, lsp_server_conf = pcall(require, "custom.plugins.lsp." .. lsp)
-
-	if ok then
-		opts = vim.tbl_deep_extend("force", opts, lsp_server_conf)
-	end
-
-	lspconfig[lsp].setup(opts)
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
 end
